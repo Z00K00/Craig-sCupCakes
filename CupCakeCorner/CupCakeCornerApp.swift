81bd1b4c52b989f22fc03aa4aa876e3442e086c0
kraiglarson2 <kraigalarson2@gmail.com>
2024-01-30 15:13:27 -0500

    Initial Commit

---
+++ CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by Krajg Larson on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
